c6fe9c12925b640858fa13787d84c4538e1b481d
Michael van Schaik <mcjvanschaik@gmail.com>
2023-06-27 20:01:20 +0200

    Changes: removed background in highlights/init.lua, configured neorg.lua further.

--- plugins/neorg.lua
+++ plugins/neorg.lua
@@ -8,19 +8,30 @@ return {
       ["core.defaults"] = {}, -- Loads default behaviour
       ["core.concealer"] = {}, -- Adds pretty icons to your documents
       ["core.keybinds"] = {}, -- Adds default keybindings
+      ["core.export"] = {},  -- Adds export functions
       ["core.completion"] = {
         config = {
           engine = "nvim-cmp",
         },
       }, -- Enables support for completion plugins
-      ["core.journal"] = {}, -- Enables support for the journal module
+      ["core.journal"] = {
+        config = {
+          strategy = "flat"
+        }
+      }, -- Enables support for the journal module
       ["core.dirman"] = { -- Manages Neorg workspaces
         config = {
           workspaces = {
             notes = "~/notes",
             remind = "~/notes/remind"
           },
+          default_workspace = "notes"
         },
+      },
+      ["core.export.markdown"] = {
+        config = {
+          extensions = "all",
+        }
       },
     },
   },
